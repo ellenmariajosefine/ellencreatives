639ea6a35d7aa266c627d1403b65ad36c61f6116
Ellen Eklööf <ekloof.ellen@gmail.com>
2020-12-02 15:53:16 +0100

    Test succesful

--- JS/filterScript.js
+++ JS/filterScript.js
@@ -41,3 +41,28 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+
+//ROTATION SCRIPT
+const boxes = document.querySelectorAll(".box");
+const range = 40;
+console.log(boxes);
+const calcValue = (a,b) => (a/b * range - range / 2).toFixed(1);
+
+let timeout;
+for (let i = 0; i < boxes.length; i++) {
+boxes[i].addEventListener('mousemove', ({x,y}) => {
+  if(timeout){
+    window.cancelAnimationFrame(timeout)
+  }
+
+  timeouot = window.requestAnimationFrame(() => {
+    const yValue = calcValue(y, window.innerHeight);
+    const xValue = calcValue(x, window.innerWidth);
+
+    boxes[i].style.transform = `rotateX(${yValue}deg) rotateY(${yValue}deg)`;
+ });
+
+}, false);
+}
