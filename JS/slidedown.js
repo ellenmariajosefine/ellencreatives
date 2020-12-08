22a658acec1ba3561baf24d4eb5a4922f5fe16f1
Ellen Eklööf <ekloof.ellen@gmail.com>
2020-12-08 19:03:01 +0100

    cases almost done2.0

---
+++ JS/slidedown.js
@@ -0,0 +1,7 @@
+$("#slidedown-btn").click(function () {
+    if ( $( "#filters" ).first().is( ":hidden" ) ) {
+      $( "#filters" ).slideDown( "slow" );
+    } else {
+      $( "#filters" ).slideUp( "slow");
+    }
+  });
