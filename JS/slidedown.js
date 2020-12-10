7a9bd916f375758aeb3d3d8d44a88c4e56c07c7f
Ellen Eklööf <ekloof.ellen@gmail.com>
2020-12-10 23:51:30 +0100

    Cases-ovoerview mobile complete media query

--- JS/slidedown.js
+++ JS/slidedown.js
@@ -1,7 +1,19 @@
+// DESKTOP + TABLET
+
 $("#slidedown-btn").click(function () {
     if ( $( ".filters" ).first().is( ":hidden" ) ) {
       $( ".filters" ).slideDown( "slow" );
     } else {
       $( ".filters" ).slideUp( "slow");
+    }
+  });
+
+
+// MOBILE
+$("#slidedown-btn-mobile").click(function () {
+    if ( $( "#filters" ).first().is( ":hidden" ) ) {
+      $( "#filters" ).slideDown( "slow" );
+    } else {
+      $( "#filters" ).slideUp( "slow");
     }
   });
